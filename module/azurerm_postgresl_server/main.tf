220d07ed8dfa644894940180d74df2dc49f6f5aa
amal4j <amal4j@gmail.com>
2026-09-27 00:14:16 +0530

    code updated

--- module/azurerm_postgresl_server/main.tf
+++ module/azurerm_postgresl_server/main.tf
@@ -1,20 +1,25 @@
 resource "azurerm_postgresql_flexible_server" "db-learning" {
-    for_each = var.db_c
-  name                          = each.value.db_name
-  resource_group_name           = each.value.resource_group_name
-  location                  = each.value.location
-  version                       = each.value.version
-#   delegated_subnet_id           = azurerm_subnet.example.id
-#   private_dns_zone_id           = azurerm_private_dns_zone.example.id
-#   public_network_access_enabled = false
-administrator_login           = "admin123"
-administrator_password        = "Admin@123"
-#   zone                          = "1"
 
-#   storage_mb   = 32768
-#   storage_tier = "P4"
+  for_each = var.db_c
 
-#   sku_name   = "B_Standard_B1ms"
-#   depends_on = [azurerm_private_dns_zone_virtual_network_link.example]
+  name                = each.value.db_name
+  resource_group_name = each.value.resource_group_name
+  location            = each.value.location
 
+  create_mode = "Default"
+
+  version = each.value.version
+
+  administrator_login    = each.value.administrator_login
+  administrator_password = each.value.administrator_password
+
+  sku_name   = each.value.sku_name
+  storage_mb = each.value.storage_mb
+
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+
+  public_network_access_enabled = true
+
+  auto_grow_enabled = false
 }
